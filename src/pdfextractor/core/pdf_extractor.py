d3aca119f37995c1058c0935db8de6a0fc3757f9
will-afs <william.afonso.pro@gmail.com>
2022-03-22 12:06:02 +0100

    Now storing pdf_data as json files through results/ folder

--- src/pdfextractor/core/pdf_extractor.py
+++ src/pdfextractor/core/pdf_extractor.py
@@ -2,6 +2,8 @@ from src.pdfextractor.core.pdf_extractor_utils import extract_pdf
 
 import celery
 import json
+from os import path
+import sys
 import toml
 from typing import List
 
@@ -17,17 +19,23 @@ app = celery.Celery(
             )
 
 @app.task
-def extract_pdf_task(pdf_metadata:dict) -> List:
-    # print("Task called with argument : " + pdf_metadata['uri'])
+def extract_pdf_task(pdf_metadata:dict, erase_file=True) -> List:
     pdf_urn = pdf_metadata['uri'].replace('http://arxiv.org/pdf/cs/', '')
-    try:
-        pdf_dict = extract_pdf(pdf_metadata, cooldown_manager_uri)
-    except:
-        return 'Extraction failed for PDF with URN \"' + pdf_urn + '\"'
-    json_object = json.dumps(pdf_dict, indent = 4)
-    with open("results/{}.json".format(pdf_urn), "w") as outfile:
-        outfile.write(json_object)
-    return 'Successfuly extracted PDF with URN \"' + pdf_urn + '\"'
+    if not erase_file and (path.exists('results/errors/'+pdf_urn) or path.exists('results/success/'+pdf_urn)):
+        return 'File \"' + pdf_urn + '\" alredy exists'
+    else:
+        try:
+            pdf_dict = extract_pdf(pdf_metadata, cooldown_manager_uri)
+        except:
+            # Storing the pdf_metadata that lead to an error to debug it
+            json_object = json.dumps(pdf_metadata, indent = 4)
+            with open("results/errors/{}.json".format(pdf_urn), "w") as outfile:
+                outfile.write(json_object)
+            return '\nExtraction failed for PDF with metadata \"' + json.dumps(pdf_metadata) + '\"\n'
+        json_object = json.dumps(pdf_dict, indent = 4)
+        with open("results/success/{}.json".format(pdf_urn), "w") as outfile:
+            outfile.write(json_object)
+        return 'Successfuly extracted PDF with URN \"' + pdf_urn + '\"'
 
 if __name__ == '__main__':
     app.start()
--- src/pdfextractor/core/pdf_extractor_utils.py
+++ src/pdfextractor/core/pdf_extractor_utils.py
@@ -57,8 +57,14 @@ def extract_pdf(pdf_metadata:dict, cooldown_manager_uri:str) -> dict:
             pass
         else:
             # extract each author from string
-            reference['authors'] = extract_authors_from_apa_ref(reference_dict['raw_ref'][0])
-            pdf_dict['references'].append(reference)
+            try:
+                reference['authors'] = extract_authors_from_apa_ref(reference_dict['raw_ref'][0])
+            except:
+                #TODO: rather use a logger.debug
+                print('\nCould not extract correctly authors '\
+                    'from the following raw_reference:\n{}\n'.format(reference_dict['raw_ref'][0]))
+            else:
+                pdf_dict['references'].append(reference)
 
     return pdf_dict
 
@@ -72,12 +78,27 @@ def extract_authors_from_apa_ref(apa_ref:str)->list:
     list: the list of authors as string elements
     """
     authors = []
-    # Searching pattern ' T. (1995)' or ' T. (1995b)' like
+    # Searching pattern 'T. (1995)' or ' T. (1995b)' like
     pattern = re.compile(r" [A-Z]\.\ \([1-2][0-9][0-9][0-9][a-z]*\)")
     # 3 = len(' T.'), which should be included in authors_string
-    end_authors_section_idx = re.search(pattern, apa_ref, flags=0).start() + 3
+    # try:
+    end_authors_section_idx = re.search(pattern, apa_ref, flags=0).start() + 1
+    # except AttributeError: # AttributeError: 'NoneType' object has no attribute 'start'
+    #     # Searching pattern 'L. (Eds.). (1977)' like
+    #     pattern = re.compile(r" [A-Z]\.\ \(Eds\.\) \([1-2][0-9][0-9][0-9][a-z]*\)")
+    #     end_authors_section_idx = re.search(pattern, apa_ref, flags=0).start() + 1
     authors_string = apa_ref[0:end_authors_section_idx]
     authors_string = authors_string.replace('& ', '')
     authors_string = authors_string.replace('.,', '..,')
     authors = authors_string.split('., ')
     return authors
+
+if __name__ == '__main__':
+    pdf_metadata = {
+        "uri": "http://arxiv.org/pdf/cs/9308102v1",
+        "title": "A Market-Oriented Programming Environment and its Application to\n  Distributed Multicommodity Flow Problems",
+        "authors": ["M. P. Wellman"]
+        }
+    coooldown_manager_uri = "http://172.17.0.2:5000/"
+    pdf_dict = extract_pdf(pdf_metadata, coooldown_manager_uri)
+    
